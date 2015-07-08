3f023dbe6217a240a9d2bb3ab5db5017e2e0b3df
Severin Schwanck <s@schwanck.com>
2015-07-08 14:26:53 +0200

    clone ids

--- app/models/cms/concerns/content_categories.rb
+++ app/models/cms/concerns/content_categories.rb
@@ -8,7 +8,7 @@ module Cms
       included do
         has_and_belongs_to_many :content_categories
 
-        scope :with_categories, -> (ids) { select('DISTINCT content_nodes.*').joins(:content_categories).where('content_category_id IN (?)', ids << -1) }
+        scope :with_categories, -> (ids) { select('DISTINCT content_nodes.*').joins(:content_categories).where('content_category_id IN (?)', ids.clone << -1) }
       end
 
     end
