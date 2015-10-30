5c6cebea913a913b00ef87ea455d88dee6ca7abf
Severin Schwanck <s@schwanck.com>
2015-10-30 12:44:54 +0100

    only return float value if its > 0

--- app/models/cms/content_attributes/float_attribute.rb
+++ app/models/cms/content_attributes/float_attribute.rb
@@ -1,4 +1,16 @@
 # encoding: utf-8
 class FloatAttribute < Cms::ContentAttribute
   content_type :float
+
+  # assgin 0.0 if value is blank
+  def value=(value)
+    assign_value(value.present? ? value : value.to_f)
+  end
+
+  # do not return 0.0 values
+  def value
+    val = fetch_value
+    val if val > 0
+  end
+
 end
