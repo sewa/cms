67bca7bc53b682f9cecf2c3cd266c734569db693
Lars Kuhnt <lars.kuhnt@gmail.com>
2019-07-24 14:10:42 +0200

    fixed broken cms navigation

--- app/assets/javascripts/cms/accordion.js
+++ app/assets/javascripts/cms/accordion.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     $(window.location.hash).addClass('active');
     $('input#dom_id').val(window.location.hash);
   }
-  if (dom_id) {
+  if (typeof variable !== 'undefined' && dom_id) {
     $(dom_id).addClass('active');
     history.replaceState(null, null, dom_id);
   }
